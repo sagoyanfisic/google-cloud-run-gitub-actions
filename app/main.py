a81e0b66b76e5215b331513ceebaa342a2f3e74a
Yancel Salinas <sagoyanfisic1@gmail.com>
2024-05-25 10:25:43 -0500

    feat(fastapi): add function generative ai

--- app/main.py
+++ app/main.py
@@ -1,11 +1,34 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+import google.generativeai as generative_ai
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
+class Settings:
+    GOOGLE_API_KEY: str = os.getenv("GOOGLE_API_KEY")
+
+settings = Settings()
+
+
+class PromptRequest(BaseModel):
+    prompt: str
+    
+generative_ai.configure(api_key=settings.GOOGLE_API_KEY)
+
+
+@app.get("health/")
+async def health():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/classify-sentiment")
+async def classify_sentimental(request: PromptRequest):
+    try:
+        model = generative_ai.GenerativeModel('gemini-1.5-pro')
+        formatted_prompt = f"Analiza el sentimiento de los siguiente frase y clasifícalos solamente como POSITIVO, NEGATIVO o NEUTRO(NO DEBES DAR MAS EXPLICACION, SOLO INDICAME POSITIVO, NEGATIVO O NEUTRO). {request.prompt}."
+        response = model.generate_content(formatted_prompt)
+        return {"response": response.text}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
